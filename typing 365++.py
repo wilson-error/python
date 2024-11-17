6800a75d94373c18f2bd629175def41a294955a3
wilson-error <wilsonli1025@outlook.com>
2024-11-17 18:28:42 +0800

    Add files via upload

---
+++ typing 365++.py
@@ -0,0 +1,291 @@
+print('you have to subscribe to typing365 plus to use this website.')
+print('1.Monthly  ¥5.99')
+print('2.Yearly  ¥10.99')
+print('3.Lifetime  ¥39.99')
+
+
+y12=int(input('which plan do you want to subscribe:'))
+if y12 == 1:
+    ycl = 5.99
+    print('please pay your subscription fee',ycl)
+elif y12 == 2:
+    ycl = 10.99
+    print('please pay your subscription fee',ycl)
+elif y12 == 3:
+    ycl = 39.99
+    print('please pay your subscription fee',ycl)
+
+
+
+import time
+print('syncing,it will take about one minute……')
+time.sleep(60)
+print('sync is done,now you can start your typing journey')
+
+    
+
+
+
+
+
+ls=['coding essentials','math essentials','26 letters','basic english words','basic chinese words','left hand practice','right hand practice']
+a=0
+while True:
+    print(a+1,ls[a])
+    a+=1
+    if a == len(ls):
+        break
+
+cnt=1
+a=0
+
+
+cd=[ 'if', 'else', 'for', 'while', 'break' ,'True']
+print('you are in the stage',cnt,'',ls[a])
+for c in cd:
+    print('lets type the word',c)
+    b = input()
+    if b == c:
+        print('you did a great job! ')
+    else:
+        print('please do it again')
+        while True:
+            print('lets type the key',c)
+            b = input()
+            if b==c:
+                break
+        continue
+cnt+= 1
+a+= 1
+
+
+
+print('do you want to take a 1 min break ?')
+print('f = yes')
+print('j = no' )
+r = input()
+if r == 'f':
+    import time
+    z=60
+    print(z)
+    while True:
+        time.sleep(1)
+        z-=1
+        print(z)
+        if z == 0:
+            break
+    print('lets move on')
+else:
+    print('you can always move on without the break')
+    
+
+
+
+mt=['real number','rational number','irrational number','fraction','integer','decimal']
+print('you are in the stage',cnt,'',ls[a])
+for m in mt:
+    print('lets type the word',m)
+    b = input()
+    if b == m:
+        print('you did a great job! ')
+    else:
+        print('please do it again')
+        while True:
+            print('lets type the key',m)
+            b = input()
+            if b==m:
+                break
+        continue
+cnt+= 1
+a+= 1
+print('do you want to take a 1 min break ?')
+print('f = yes')
+print('j = no' )
+r = input()
+if r == 'f':
+    import time
+    z=60
+    print(z)
+    while True:
+        time.sleep(1)
+        z-=1
+        print(z)
+        if z == 0:
+            break
+    print('lets move on')
+else:
+    print('you can always move on without the break')        
+        
+
+ls1 = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+print('you are in the stage',cnt,'',ls[a])
+for a in ls1:
+    print('lets type the key',a)
+    b = input()
+    if b==a:
+        print('lets move on! ')
+    else:
+        print('lets try again')
+        while True:
+            print('lets type the key',a)
+            b = input()
+            if b==a:
+                break
+        continue
+cnt+= 1
+a+=1
+print('do you want to take a 1 min break ?')
+print('f = yes')
+print('j = no' )
+r = input()
+if r == 'f':
+    import time
+    z=60
+    print(z)
+    while True:
+        time.sleep(1)
+        z-=1
+        print(z)
+        if z == 0:
+            break
+    print('lets move on')
+else:
+    print('you can always move on without the break')
+
+
+
+
+words=['stairs','role','interior','surface','word','chamber','core','typing']
+print('you are in the stage',cnt,'',ls[a])
+
+for w in words:
+    print('lets type the word',w)
+    b = input()
+    if b == w:
+        print('you did a great job! ')
+    else:
+        print('please do it again')
+        while True:
+            print('lets type the key',w)
+            b = input()
+            if b==w:
+                break
+        continue
+cnt+= 1
+a+= 1
+print('do you want to take a 1 min break ?')
+print('f = yes')
+print('j = no' )
+r = input()
+if r == 'f':
+    import time
+    z=60
+    print(z)
+    while True:
+        time.sleep(1)
+        z-=1
+        print(z)
+        if z == 0:
+            break
+    print('lets move on')
+else:
+    print('you can always move on without the break')
+
+
+
+CNwords = ['第','便','叫','有','洗','母']
+print('you are in the stage',cnt,'',ls[a])
+print('use the chinese keyboard layout and type the pinyin of the word!')
+for c in CNwords:
+    print('lets type the chinese word',c)
+    b = input()
+    if b==c:
+        print('lets move on! ')
+    else:
+        print('lets try again')
+        while True:
+            print('lets type the chinese word',c)
+            b = input()
+            if b==c:
+                break
+        continue
+cnt+= 1
+a+= 1
+print('do you want to take a 1 min break ?')
+print('f = yes')
+print('j = no' )
+r = input()
+if r == 'f':
+    import time
+    z=60
+    print(z)
+    while True:
+        time.sleep(1)
+        z-=1
+        print(z)
+        if z == 0:
+            break
+    print('lets move on')
+else:
+    print('you can always move on without the break')
+
+
+
+left=['rastawas','rebeccawas','edgarwas','strawberry','cat','baste']
+print('you are in the stage',cnt,'',ls[a])
+
+for l in left:
+    print('lets type the word',l)
+    b = input()
+    if b == l:
+        print('you did a great job! ')
+    else:
+        print('please do it again')
+        while True:
+            print('lets type the key',l)
+            b = input()
+            if b==l:
+                break
+        continue
+cnt+= 1
+a+= 1
+print('do you want to take a 1 min break ?')
+print('f = yes')
+print('j = no' )
+r = input()
+if r == 'f':
+    import time
+    z=60
+    print(z)
+    while True:
+        time.sleep(1)
+        z-=1
+        print(z)
+        if z == 0:
+            break
+    print('lets move on')
+else:
+    print('you can always move on without the break')
+
+
+
+
+right=['kilo','jump','poll','ill','hill','puppy']
+print('you are in the stage',cnt,'',ls[a])
+
+for r in right:
+    print('lets type the word',r)
+    b = input()
+    if b == r:
+        print('you did a great job! ')
+    else:
+        print('please do it again')
+        while True:
+            print('lets type the key',r)
+            b = input()
+            if b==r:
+                break
+        continue
+
+
+print('congratulations, you have learned everything about typing, but you can always come back to practice and increase your typing skills.')
